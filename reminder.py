5db646f321fac0180fa04490cb8aaabd264bdc61
slydesigns <sylvestorsielei@gmail.com>
2023-02-28 20:28:50 +0300

    email details

---
+++ reminder.py
@@ -0,0 +1,11 @@
+import psutil
+import time
+import smtplib
+from email.mime.text import MIMEText
+
+# Set up email details
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 587
+EMAIL_FROM = 'sylvestorsielei@gmail.com'
+EMAIL_PASSWORD = '0710730535'
+EMAIL_TO = 'silvestorsielei@gmail.com'
