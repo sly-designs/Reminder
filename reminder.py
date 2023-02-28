126081a18aa984284f964d7c877447841e0eaf67
slydesigns <sylvestorsielei@gmail.com>
2023-02-28 20:32:41 +0300

    check if laptop is on

--- reminder.py
+++ reminder.py
@@ -9,3 +9,13 @@ SMTP_PORT = 587
 EMAIL_FROM = 'sylvestorsielei@gmail.com'
 EMAIL_PASSWORD = '0710730535'
 EMAIL_TO = 'silvestorsielei@gmail.com'
+
+# Check if laptop is on
+def is_laptop_on():
+    battery = psutil.sensors_battery()
+    if battery is not None:
+        plugged = battery.power_plugged
+        percent = battery.percent
+        return plugged or percent > 5
+    else:
+        return True
